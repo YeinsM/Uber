1885c15ae2a5ad6ad7e972c931e346773678c367
QueenMP <queenmalagonp@gmail.com>
2021-12-09 11:17:02 -0500

    registro usuario, pestaña usuarios y estaciones

---
+++ src/app/servicios/estaciones.service.ts
@@ -0,0 +1,72 @@
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { EstacionModelo } from '../modelos/estacion.model';
+import { SeguridadService } from './seguridad.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EstacionesService {
+
+  url = "http://localhost:3000"
+  token: string = ''
+
+  constructor(private http: HttpClient,
+    private seguridadService: SeguridadService) {
+    this.token = this.seguridadService.getToken();
+  }
+
+  getAll(): Observable<EstacionModelo[]> {
+    return this.http.get<EstacionModelo[]>(`${this.url}/estaciones`, {
+      headers: new HttpHeaders({
+        "Authorization": `Bearer ${this.token}`
+      })
+    })
+  }
+
+  store(estacion: EstacionModelo): Observable<EstacionModelo> {
+    return this.http.post<EstacionModelo>(`${this.url}/estaciones`, {
+      nombre: estacion.nombre,
+      direccion: estacion.direccion,
+      coordenada_x: estacion.coordenada_x,
+      coordenada_y: estacion.coordenada_y,
+      tipo: estacion.tipo
+    },{
+      headers: new HttpHeaders({
+        "Authorization": `Bearer ${this.token}`
+      })
+    });
+  }
+
+  update(estacion: EstacionModelo): Observable<EstacionModelo> {
+    return this.http.patch<EstacionModelo>(`${this.url}/estaciones/${estacion.id}`, {
+      nombre: estacion.nombre,
+      direccion: estacion.direccion,
+      coordenada_x: estacion.coordenada_x,
+      coordenada_y: estacion.coordenada_y,
+      tipo: estacion.tipo
+    }, {
+      headers: new HttpHeaders({
+        "Authorization": `Bearer ${this.token}`
+      })
+    });
+    
+  }
+
+  delete(id: string): Observable<EstacionModelo[]> {
+    return this.http.delete<EstacionModelo[]>(`${this.url}/estaciones/${id}`, {
+      headers: new HttpHeaders({
+        "Authorization": `Bearer ${this.token}`
+      })
+    })
+  }
+
+  getWithId(id: string): Observable<EstacionModelo> {
+    return this.http.get<EstacionModelo>(`${this.url}/estaciones/${id}`, {
+      headers: new HttpHeaders({
+        "Authorization": `Bearer ${this.token}`
+      })
+    })
+  }
+}
